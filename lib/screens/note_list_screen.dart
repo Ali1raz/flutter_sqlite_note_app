dd502fdd6f3c2874d9363242be4913bc388f5b9b
Ali Raza <alirazakhalid1129@gmail.com>
2025-03-23 11:33:19 +0500

    add basic listview and floatingActionButton

--- lib/screens/note_list_screen.dart
+++ lib/screens/note_list_screen.dart
@@ -8,12 +8,45 @@ class NoteListScreen extends StatefulWidget {
 }
 
 class _NoteListScreenState extends State<NoteListScreen> {
+  int count = 2;
+  ListView _getListView() {
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          color: Colors.white,
+          elevation: 2,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.deepPurple,
+              child: Icon(Icons.note),
+            ),
+            title: Text("Note 1"),
+            subtitle: Text("This is a note"),
+            trailing: Icon(Icons.delete),
+            onTap: () {
+              debugPrint("onTap ListTile");
+            },
+          ),
+        );
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Notes"),
           elevation: 8,
+      ),
+      body: _getListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint("FloatingActionButton onPressed");
+        },
+        tooltip: "Add Note",
+        child: Icon(Icons.add),
       ),
     );
   }
